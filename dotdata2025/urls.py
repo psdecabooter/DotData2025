d74d993f4a4ac9346c10395a03a754ce9c2d9304
Patrick DeCabooter <psdecabooter@gmail.com>
2025-02-22 22:41:53 -0600

    Created the base infrastructure for the home view

--- dotdata2025/urls.py
+++ dotdata2025/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
     path('solar/', include('solar.urls')),
+    path('', include('core.urls')),
 ]
