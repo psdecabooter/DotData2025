941ff59815df07dcf4e861d36aa54eda9ace576d
Srinivas I B <srinivasib@Srinivass-MacBook-Pro.local>
2025-02-23 11:11:18 -0600

    minor changes

--- dotdata2025/settings.py
+++ dotdata2025/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tmk5^@#qg1*xw)fqk3()o_x)zx(y-xm+pzkn+78#u&232jsw8s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
