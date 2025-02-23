de0fb7b0fd36f19fb9584602c3f49ff7b8a65ee7
theperfectbanana <maeric567@gmail.com>
2025-02-22 22:08:22 -0600

    Create a basic front end accessable via http://127.0.0.1:8000/solar/radiation/

--- dotdata2025/settings.py
+++ dotdata2025/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'solar',
     'core',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+
 ]
 
 ROOT_URLCONF = 'dotdata2025.urls'
@@ -71,6 +74,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dotdata2025.wsgi.application'
 
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
