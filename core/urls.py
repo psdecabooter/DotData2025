a1b37f6955e779d772cea06d9f6b62749989f69c
Patrick DeCabooter <psdecabooter@gmail.com>
2025-02-23 01:32:10 -0600

    Bare bones view of the home page completed

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.homeview)
+    path('', views.homeview, name="home"),
+    path('estimate/', views.estimateview, name="estimate")
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homeview(request):
     return render(request, 'home.html', {})
+
+def estimateview(request):
+    return render(request, 'estimate.html', {})
