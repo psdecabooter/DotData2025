7ad8d87eb3a7c8716659eec5f97d6711e7f1671e
Patrick DeCabooter <psdecabooter@gmail.com>
2025-02-23 03:35:31 -0600

    Finished the functionality of the main page

--- core/views.py
+++ core/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def homeview(request):
-    return render(request, 'home.html', {})
+    # default values
+    parameters = {'latitude':37.7749, 'longitude':-122.4194, 'stateName':'Unknown'}
+
+    if request.method == "POST":
+        print(request.POST.get("latitude"))
+        print(request.POST.get("longitude"))
+        print(request.POST.get("stateName"))
+        request.session['latitude'] = request.POST.get("latitude")
+        request.session['longitude'] = request.POST.get("longitude")
+        request.session['stateName'] = request.POST.get("stateName")
+        if request.session['stateName'] != "Unknown":
+            return redirect('estimate/')
+            #return estimateview(request)
+
+    if 'latitude' in request.session and 'longitude' in request.session and 'stateName' in request.session:
+        parameters = {'latitude':request.session['latitude'], 'longitude':request.session['longitude'], 'stateName':request.session['stateName']} 
+    
+    return render(request, 'home.html', parameters)
 
 def estimateview(request):
     return render(request, 'estimate.html', {})
